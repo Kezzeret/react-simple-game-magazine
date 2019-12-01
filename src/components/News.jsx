694ed50b01da90203dc0a05bcd3b10d7a1014057
Kezzeret <56687405+Kezzeret@users.noreply.github.com>
2019-12-02 02:28:54 +0300

    021219
    -

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import './News.css';
-import New from './New.jsx'
+
+import PieceOfNews from './PieceOfNews.jsx'
 
 const News = () => {
+    let newsData = [
+        {preview: 'https://cdn.everyplayer.ru/source/1/AxlQOLHYrt_76tfaDUqbTQVvFqYPj81P.jpg', title: 'Азерот здесь', date: '01 августа 1999', like_count: 'никому не нравится', text: 'Готовьтесь к грандиозному возвращению королевы волн 26 июня в обновлении «Возвращение Азшары»!', links: 'ссылки на соцсети', more: 'читать подробнее'},
+        {preview: 'https://cdn.everyplayer.ru/source/1/AxlQOLHYrt_76tfaDUqbTQVvFqYPj81P.jpg', title: 'Азерот там', date: '01 августа 1999', like_count: 'никому не нравится', text: 'Готовьтесь к грандиозному возвращению королевы волн 26 июня в обновлении «Возвращение Азшары»!', links: 'ссылки на соцсети', more: 'читать подробнее'},
+        {preview: 'https://cdn.everyplayer.ru/source/1/AxlQOLHYrt_76tfaDUqbTQVvFqYPj81P.jpg', title: 'Азерот сям', date: '01 августа 1999', like_count: 'никому не нравится', text: 'Готовьтесь к грандиозному возвращению королевы волн 26 июня в обновлении «Возвращение Азшары»!', links: 'ссылки на соцсети', more: 'читать подробнее'},
+        {preview: 'https://cdn.everyplayer.ru/source/1/AxlQOLHYrt_76tfaDUqbTQVvFqYPj81P.jpg', title: 'Азерот здесь', date: '01 августа 1999', like_count: 'никому не нравится', text: 'Готовьтесь к грандиозному возвращению королевы волн 26 июня в обновлении «Возвращение Азшары»!', links: 'ссылки на соцсети', more: 'читать подробнее'},
+        {preview: 'https://cdn.everyplayer.ru/source/1/AxlQOLHYrt_76tfaDUqbTQVvFqYPj81P.jpg', title: 'Азерот там', date: '01 августа 1999', like_count: 'никому не нравится', text: 'Готовьтесь к грандиозному возвращению королевы волн 26 июня в обновлении «Возвращение Азшары»!', links: 'ссылки на соцсети', more: 'читать подробнее'},
+        {preview: 'https://cdn.everyplayer.ru/source/1/AxlQOLHYrt_76tfaDUqbTQVvFqYPj81P.jpg', title: 'Азерот сям', date: '01 августа 1999', like_count: 'никому не нравится', text: 'Готовьтесь к грандиозному возвращению королевы волн 26 июня в обновлении «Возвращение Азшары»!', links: 'ссылки на соцсети', more: 'читать подробнее'},
+        {preview: 'https://cdn.everyplayer.ru/source/1/AxlQOLHYrt_76tfaDUqbTQVvFqYPj81P.jpg', title: 'Азерот здесь', date: '01 августа 1999', like_count: 'никому не нравится', text: 'Готовьтесь к грандиозному возвращению королевы волн 26 июня в обновлении «Возвращение Азшары»!', links: 'ссылки на соцсети', more: 'читать подробнее'},
+        {preview: 'https://cdn.everyplayer.ru/source/1/AxlQOLHYrt_76tfaDUqbTQVvFqYPj81P.jpg', title: 'Азерот там', date: '01 августа 1999', like_count: 'никому не нравится', text: 'Готовьтесь к грандиозному возвращению королевы волн 26 июня в обновлении «Возвращение Азшары»!', links: 'ссылки на соцсети', more: 'читать подробнее'},
+        {preview: 'https://cdn.everyplayer.ru/source/1/AxlQOLHYrt_76tfaDUqbTQVvFqYPj81P.jpg', title: 'Азерот сям', date: '01 августа 1999', like_count: 'никому не нравится', text: 'Готовьтесь к грандиозному возвращению королевы волн 26 июня в обновлении «Возвращение Азшары»!', links: 'ссылки на соцсети', more: 'читать подробнее'}
+    ]
+
+    let newsElement = newsData.map( n => 
+        <PieceOfNews preview={n.preview} title={n.title} date={n.date} like_count={n.like_count} text={n.text} links={n.links} more={n.more} />
+    );
+
     return <div className='news'>
-        <New />
-        <New />
-        <New />
-        <New />
-        <New />
-        <New />
-    </div>
+        {newsElement}
+  </div>
 }
 
 export default News;
---
+++ src/components/PieceOfNews.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './PieceofNews.css';
+
+const PieceOfNews = (props) => {
+    return <div className='new'>
+    <div className='new_preview'>
+        <img src={props.preview}></img>
+    </div>
+    <div className='new_name'>
+        {props.title}
+    </div>
+    <div className='new_date'>
+        {props.date}
+    </div>
+    <div className='new_like'>
+        {props.like_count}
+    </div>
+    <div className='new_text'>
+        {props.text}
+    </div>
+    <div className='new_links'>
+        {props.links}
+    </div>
+    <div className='new_read'>
+        {props.more}
+    </div>
+  </div>
+}
+
+
+
+
+export default PieceOfNews;
